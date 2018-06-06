40fcd38f4e6db61e19bd65019091a855e54cb27d
sky <sky@skydeMacBook-Pro.local>
2018-06-07 00:36:06 +0800

    edit user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,8 +35,22 @@ class UserController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
-    public function logout()
+    public function edit(User $user)
     {
-        
+        return view ('user.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        $user->update([
+            'name'=>$request->name,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show',$user->id);
     }
 }
